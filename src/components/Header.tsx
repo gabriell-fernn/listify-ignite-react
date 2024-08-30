39d5dd1a1d465a03c6eedcb715b2c3c44c142906
Gabriell <gabriellfertnandess1@gmail.com>
2024-08-30 09:53:13 -0300

    initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import logo from '../assets/logo-todo.svg'
+
+export function Header() {
+    return (
+        <div>
+            <header>
+                <img src={logo} alt="" />
+            </header>
+        </div>
+    )
+}
